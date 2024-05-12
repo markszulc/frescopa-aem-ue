2bb0e25c8a9111817fa24ef8eeb2f09a0a5d4717
Mark Szulc <mszulc@adobe.com>
2024-05-13 00:22:09 +1000

    offer styling

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -2,7 +2,7 @@
 export default async function decorate(block) {
   const aempublishurl = 'https://publish-p123917-e1220159.adobeaemcloud.com';
   const aemauthorurl = 'https://author-p123917-e1220159.adobeaemcloud.com';
-  const persistedquery = '/graphql/execute.json/securbank/OfferByPath';
+  const persistedquery = '/graphql/execute.json/frescopa/OfferByPath';
   const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
   const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML.trim();
 
@@ -27,13 +27,15 @@ export default async function decorate(block) {
 
   block.innerHTML = `
   <div class='banner-content' data-aue-resource=${itemId} data-aue-type="reference" data-aue-filter="cf">
-      <div data-aue-prop="heroImage" data-aue-type="media" class='banner-detail' style="background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%) ,url(${aempublishurl + cfReq.heroImage._dynamicUrl});">
-          <p data-aue-prop="headline" data-aue-type="text" class='pretitle'>${cfReq.headline}</p>
-          <p data-aue-prop="pretitle" data-aue-type="text" class='headline'>${cfReq.pretitle}</p>
-          <p data-aue-prop="detail" data-aue-type="richtext" class='detail'>${cfReq.detail.plaintext}</p>
+    <div class='banner-detail'>
+      <h3 data-aue-prop="headline" data-aue-type="text" class='pretitle'>${cfReq.headline}</h3>
+      <p data-aue-prop="detail" data-aue-type="richtext" class='detail'>${cfReq.detail.plaintext}</p>
+    </div>
+    <div class='banner-image'> 
+      <div class='offer-image'>
+        <img data-aue-prop="image" data-aue-type="image" src='${cfReq.heroImage._publishUrl}' alt='Nice image'>
       </div>
-      <div class='banner-logo'>
-      </div>
+    </div>
   </div>
 `;
 }
