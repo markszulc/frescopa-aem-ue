253c2d52c8591b92b458eca29551a6128a6eebe6
Mark Szulc <mszulc@adobe.com>
2024-05-15 09:55:06 +1000

    Offer styling adjustments

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -24,15 +24,16 @@ export default async function decorate(block) {
   const itemId = `urn:aemconnection:${offerpath}/jcr:content/data/master`;
 
   block.innerHTML = `
-  <div class='banner-content' data-aue-resource=${itemId} data-aue-type="reference" data-aue-filter="cf">
-    <div class='banner-detail'>
-      <h3 data-aue-prop="headline" data-aue-type="text" class='pretitle'>${cfReq.headline}</h3>
-      <p data-aue-prop="detail" data-aue-type="richtext" class='detail'>${cfReq.detail.plaintext}</p>
-    </div>
-    <div class='banner-image'> 
-      <div class='offer-image'>
+  <div class='offer-content' data-aue-resource=${itemId} data-aue-type="reference" data-aue-filter="cf">
+    <div class='offer-background'> 
         <img data-aue-prop="image" data-aue-type="image" src='${cfReq.heroImage._publishUrl}' alt='Nice image'>
+    </div>
+    <div class='offer-foreground'>
+      <div class='message'>
+        <h3 data-aue-prop="headline" data-aue-type="text" class='pretitle'>${cfReq.headline}</h3>
+        <p data-aue-prop="detail" data-aue-type="richtext" class='detail'>${cfReq.detail.plaintext}</p>
       </div>
+      <div class='cta'><a href="#" title="Take Our Coffee Quiz" class="button secondary">${cfReq.callToAction}</a></div>
     </div>
   </div>
 `;
