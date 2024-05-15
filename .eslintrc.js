cc0a50286ef6956124e061046ea9f1c8e0b093a8
Mark Szulc <mszulc@adobe.com>
2024-05-15 14:37:19 +1000

    quotes off

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     // allow reassigning param
     'no-param-reassign': [2, { props: false }],
-    "prettier/prettier": ["error", { "singleQuote": true }],
+    "quotes": "off",
     'linebreak-style': ['error', 'unix'],
     'import/extensions': ['error', {
       js: 'always',
