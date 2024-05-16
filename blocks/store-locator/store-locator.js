4771e96c5a9401d2e1463db2d04b98e62df1c0cb
Mark Szulc <mszulc@adobe.com>
2024-05-16 19:53:18 +1000

    Delayed Map

--- blocks/store-locator/store-locator.js
+++ blocks/store-locator/store-locator.js
@@ -1,14 +1,51 @@
-import { h, render } from '../../scripts/preact.js';
-import htm from '../../scripts/htm.js';
-import LocationMap from './locationMap.js';
 
-// Initialize htm with Preact
-const html = htm.bind(h);
+
+
+export async function initMap() {
+  // eslint-disable-next-line no-undef
+  const { Map } = await google.maps.importLibrary('maps');
+
+  map = new Map(document.getElementById('locator-map'), {
+    center: {
+      lat: 36.2425741,
+      lng: -113.7464011,
+    },
+    zoom: 8,
+  });
+  // eslint-disable-next-line no-undef
+  infoWindow = new google.maps.InfoWindow({
+    map,
+  });
+  getMyLocation(false);
+}
+
+
 
 export default function decorate(block) {
 
   const pText = block.querySelector('p').textContent;
-  block.innerHTML = '';
-  render(html`<${LocationMap} heading=${pText} />`, block);
+  block.textContent = '';
+  
+  window.initMap = async () => {
+    initMap();
+  };
+
+  const d = document.createElement('div');
+  d.className = 'map-container';
+  const mdiv = document.createElement('div');
+  mdiv.id = 'locator-map';
+  mdiv.className = 'map';
+  d.append(mdiv);
+  const rdiv = document.createElement('div');
+  rdiv.id = 'locator-results';
+  rdiv.className = 'results';
+  d.appendChild(rdiv);
+  
+  const spinner = document.createElement('div');
+  spinner.classList.add('loading-modal');
+  spinner.style.display = 'none';
+  d.appendChild(spinner);
+
+  block.append(d);
 
 }
